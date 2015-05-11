b1e45c0a7d3cb15dfdb7dedb849fd9b2803d7431
qqyoungqq <qianyang@mail.usf.edu>
2015-05-10 20:43:25 -0400

    style: include the "use strict;" tag

--- js/resources.js
+++ js/resources.js
@@ -1,3 +1,6 @@
+// Whole-script strict mode syntax
+"use strict";
+
 /* Resources.js
  * This is simple an image loading utility. It eases the process of loading
  * image files so that they can be used within your game. It also includes
